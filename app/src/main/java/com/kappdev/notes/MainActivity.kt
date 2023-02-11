d3c0ef417fa1badff9dc8adb5d89f90d1eea640a
Kolya <kola13r@gmail.com>
2023-02-11 15:59:54 +0200

    version 0.0.0

---
+++ app/src/main/java/com/kappdev/notes/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.kappdev.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.kappdev.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
